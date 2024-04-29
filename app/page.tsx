e0c4c6c639a4fe9f9f79562bbaa2b8408dbfcca6
aych <aychdev1@gmail.com>
2024-04-29 17:49:49 -0400

    removed imports not being used

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { useWallet } from '@solana/wallet-adapter-react';
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
